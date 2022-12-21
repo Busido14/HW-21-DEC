5fac5ee6327236b1264b2197327b314b5811ae1e
Artem <artchzhen@gmail.com>
2022-12-22 01:40:18 +0500

    intit devolopment

---
+++ HW 21 Dec UIKit/HW 21 Dec UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 21 Dec UIKit
+//
+//  Created by Артем Чжен on 22/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
